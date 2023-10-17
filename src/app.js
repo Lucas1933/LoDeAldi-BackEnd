72890e98150acef2e91c9b43fdb4518625e3e4fb
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-10-17 20:54:02 -0300

    add basic self restaring server, workers and cluster

--- src/app.js
+++ src/app.js
@@ -10,33 +10,55 @@ import LoginRouter from "./routes/login_router.js";
 
 import errorHandler from "./middleware/errorHandler.js";
 
-const app = express();
-const port = config.app.PORT;
+import cluster from "cluster";
+import os from "os";
 
-app.use(
-  cors({
-    origin: config.app.API_URL,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
-const foodRouter = new FoodRouter();
-const typeRouter = new TypeRouter();
-const comboRouter = new ComboRouter();
-const loginRouter = new LoginRouter();
+const cpus = os.cpus().length;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+if (cluster.isPrimary) {
+  console.log(
+    `Principal process, starting workers.` +
+      "\n" +
+      `Lo De Aldi API listening on: ${config.app.PORT}`
+  );
 
-app.use("/lodealdi-api/v1/food", foodRouter.getRouter());
-app.use("/lodealdi-api/v1/type", typeRouter.getRouter());
-app.use("/lodealdi-api/v1/combo", comboRouter.getRouter());
-app.use("/lodealdi-api/v1/login", loginRouter.getRouter());
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker) => {
+    console.log(`The worker ${worker.process.pid} died`);
+    console.log("Creating new worker");
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  const port = config.app.PORT;
 
-connectToDatabase(config.mongo.URL);
+  app.use(
+    cors({
+      origin: config.app.API_URL,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    })
+  );
+  const foodRouter = new FoodRouter();
+  const typeRouter = new TypeRouter();
+  const comboRouter = new ComboRouter();
+  const loginRouter = new LoginRouter();
 
-app.use(errorHandler);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Lo De Aldi API listening on: ${port}`);
-});
+  app.use("/lodealdi-api/v1/food", foodRouter.getRouter());
+  app.use("/lodealdi-api/v1/type", typeRouter.getRouter());
+  app.use("/lodealdi-api/v1/combo", comboRouter.getRouter());
+  app.use("/lodealdi-api/v1/login", loginRouter.getRouter());
+
+  connectToDatabase(config.mongo.URL);
+
+  app.use(errorHandler);
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} is running`);
+  });
+}
